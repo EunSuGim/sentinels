f6bead67da78efffb6c9d0979c0d24a77abee29e
EunSuGim <kssamh883@naver.com>
2022-03-09 22:38:31 +0900

    <feat>: application.class 추가

---
+++ src/main/java/com/sentinels/manage/Application.java
@@ -0,0 +1,12 @@
+package com.sentinels.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
